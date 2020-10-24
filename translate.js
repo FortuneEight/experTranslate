52d8e4c455e6678414c1d4fa2b63563d5d27f570
Miguel <miko3rd09@gmail.com>
2020-10-24 09:46:46 -0400

    scaling, rotation, and translating tests

--- translate.js
+++ translate.js
@@ -1,7 +1,34 @@
+let rotation = 0;
+let scaling = 1;
+
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(500, 500);
+    rectMode(CENTER);
+    // frameRate(100);
 }
 
 function draw() {
-  background(220);
+    scaling = 1;
+    push();
+    translate(75, 75);
+    background(220);
+    scale(scaling);
+
+    for (let i = 0; i < 10; i++) {
+        rotate(rotation);
+        square(0, 0, 50);
+        rotation += 0.005;
+        scaling += .02;
+        scale(scaling);
+    }
+
+    pop();
+    translate(425, 425);
+
+    for (let i = 0; i < 2; i++) {
+        rotate(rotation);
+        square(0, 0, 50);
+        rotation += 0.0005;
+    }
+
 }
